f0b0c51500ade8fd2d7b37c446d17cf41e17c74b
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-21 10:05:11 -0400

    :sparkles: feat: Add "RouterProvider"

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
 
 import GlobalStyle from "./shared/styles/Global";
+import { Rotas } from "./routes";
+import { RouterProvider } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Rotas} />
     <GlobalStyle />
   </React.StrictMode>
 );
