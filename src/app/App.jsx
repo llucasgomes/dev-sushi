9291b1b6274f4dab9f9c706fc764649a2dca6bfa
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-20 23:56:40 -0400

    :sparkles: chore: Add "NewLetter"

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,16 +2,18 @@ import { About } from "../shared/components/About";
 import { Foods } from "../shared/components/Foods";
 import { Hero } from "../shared/components/Hero";
 import { NavBar } from "../shared/components/Navbar";
+import { NewLetter } from "../shared/components/NewLetter";
 import { Trending } from "../shared/components/Trending";
 
 function App() {
   return (
     <>
-       <NavBar />
+      <NavBar />
       <Hero />
       <About />
       <Foods />
       <Trending />
+      <NewLetter />
     </>
   );
 }
