59c1879e590ab523ee19021cb5e81337a06038fd
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-21 10:05:44 -0400

    :sparkles: feat: Page Home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,21 @@
+import { About } from "../../shared/components/About";
+import { Foods } from "../../shared/components/Foods";
+import { Footer } from "../../shared/components/Footer";
+import { Hero } from "../../shared/components/Hero";
+import { NavBar } from "../../shared/components/Navbar";
+import { NewLetter } from "../../shared/components/NewLetter";
+import { Trending } from "../../shared/components/Trending";
+
+export const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Foods />
+      <Trending />
+      <NewLetter />
+      <Footer />
+    </>
+  );
+};
