d9a1cd38f771311e48676e3233ee612190b50b6f
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2023-06-22 11:00:13 -0400

    :sparkles: feat: Create BD

---
+++ src/assets/data/images.js
@@ -0,0 +1,53 @@
+import Danggoo from "../images/types-pratos/danggo.png";
+import Ramen from "../images/types-pratos/ramen.png";
+import Sushi from "../images/types-pratos/sushi.png";
+import Udon from "../images/types-pratos/udon.png";
+
+import Carrousel_Sushi from "../images/carrousel/sushi.png";
+import Carrousel_Chezu from "../images/carrousel/chezu.png";
+import Carrousel_Ramen from "../images/carrousel/ramen.png";
+
+export const Imagens = [
+  {
+    types_pratos: [
+      {
+        name: "Danggoo",
+        img: Danggoo,
+      },
+      {
+        name: "Ramen",
+        img: Ramen,
+      },
+      {
+        name: "Sushi",
+        img: Sushi,
+      },
+      {
+        name: "Udon",
+        img: Udon,
+      },
+    ],
+  },
+  {
+    carrousel: [
+      {
+        title_prato: "Chezu Sushi",
+        img: Carrousel_Sushi,
+        point: "4.7",
+        price: "16.00",
+      },
+      {
+        title_prato: "Originale Sushi",
+        img: Carrousel_Chezu,
+        point: "4.8",
+        price: "21.00",
+      },
+      {
+        title_prato: "Ramen Legendo",
+        img: Carrousel_Ramen,
+        point: "4.7",
+        price: "16.00",
+      },
+    ],
+  },
+];
